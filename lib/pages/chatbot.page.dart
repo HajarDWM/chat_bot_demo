49452c8408c64dbe70be61028eea509529302076
HajarDWM <Hajar.phy@gmail.com>
2025-05-22 16:07:18 +0100

    feat: Implement ChatbotPage UI
    This commit introduces the `ChatbotPage` widget, which provides a user interface for a chatbot.
    
    The page includes:
    - An AppBar with the title "Chatbot DWM".
    - A scrollable list view to display chat messages.
    - Each message in the list shows the message content, the sender type ("user" or "bot"), and an icon representing the sender.
    - User messages are indicated with a person icon and blue color.
    - Bot messages are indicated with a chat icon and green color.
    - A text input field at the bottom for the user to type messages, labeled "Username" with a person icon.
    
    Currently, the chat messages are hardcoded for demonstration purposes.

---
+++ lib/pages/chatbot.page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class ChatbotPage extends StatelessWidget {
+  ChatbotPage({super.key});
+  var messages = [
+    {"type": "user", "message": "Hello"},
+    {"type": "bot", "message": "Hi, how can I help you?"},
+    {"type": "user", "message": "What is your name?"},
+    {"type": "bot", "message": "I am a chatbot."},
+    {"type": "user", "message": "What can you do?"},
+    {"type": "bot", "message": "I can chat with you."},
+  ];
+  TextEditingController usernameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Chatbot DWM",
+          style: TextStyle(color: Theme.of(context).indicatorColor),
+        ),
+
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${messages[index]['message']}"),
+                  subtitle: Text("${messages[index]['type']}"),
+                  leading: Icon(
+                    messages[index]['type'] == 'user'
+                        ? Icons.person
+                        : Icons.chat,
+                    color:
+                        messages[index]['type'] == 'user'
+                            ? Colors.blue
+                            : Colors.green,
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      suffixIcon: Icon(Icons.person),
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
